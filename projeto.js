23499d2a70e4bb951d8a524a8d28da4c8356fb7e
Leonardo josé oliveira bezerra <Leonardo jos oliveira bezerra>
2017-02-03 20:05:23 -0200

    Hello world

---
+++ projeto.js
@@ -0,0 +1,14 @@
+const caixaEletronico = {
+  cem: '100',
+  cinquenta:'50',
+  vinte: '20',
+  dez: '10'
+};
+
+const getMoney = (saque) => {
+  if(saque >= 100) {
+    const divby100 =   Math.trunc(saque / 100);
+    }
+};
+getMoney(150);
+console.log(divby100)
